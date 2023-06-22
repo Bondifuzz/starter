60279e79623a7fa2cd4499feb94f751146e124cf
Pavel Knyazev <poulix.nova@mail.ru>
2023-06-22 23:44:34 +0300

    Remove unused scripts

--- scripts/rmcred.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp ./local/dotenv ./.env.backup
-sed -i '/YC_API_ACCESS_KEY=/c\YC_API_ACCESS_KEY=<your-value>' ./local/dotenv
-sed -i '/YC_API_SECRET_KEY=/c\YC_API_SECRET_KEY=<your-value>' ./local/dotenv
